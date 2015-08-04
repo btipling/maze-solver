57a6297993fdbe40692afd7c257bd63e0f476c47
Bjorn Tipling <bjorn@ambientchill.com>
2015-08-03 19:55:50 -0700

    Progress on creating node graph.

--- src/dijkstra/objects/algorithms/Dijkstra.kt
+++ src/dijkstra/objects/algorithms/Dijkstra.kt
@@ -2,10 +2,70 @@ package dijkstra.objects.algorithms
 
 import dijkstra.objects.Grid
 import dijkstra.objects.Point
+import java.util.*
 
 
 public class Dijkstra : SearchAlgorithm {
+    class Node(marker: Grid.Marker) {
+        public var totalDistance: Int = -1
+        public val marker: Grid.Marker = marker
+        public val edges: MutableSet<Node> = HashSet()
+    }
     override fun execute(grid: Grid): List<Point> {
-        throw UnsupportedOperationException()
+        val nodes = createNodes(grid)
+        val startNode = createNodeGraph(nodes, grid) ?: return ArrayList<Point>()
+        val unvisited = nodesToSet(nodes)
+        return findPath(startNode, unvisited, grid)
+    }
+
+    private fun createNodes(grid: Grid): ArrayList<Node> {
+        val nodes = ArrayList<Node>()
+        for (i in 0..(grid.columns-1)) {
+            for (j in 0..(grid.rows-1)) {
+                nodes.add(Node(grid.get(i, j)))
+            }
+        }
+        return nodes
+    }
+
+    private fun getIndexFromPoint(point: Point, grid: Grid): Int {
+        return point.y * grid.rows + point.x
+    }
+
+    private fun getPointFromIndex(index: Int, grid: Grid): Point {
+        return Point(index/grid.rows, index % grid.rows)
+    }
+
+    private fun createNodeGraph(nodes: ArrayList<Node>, grid: Grid): Node? {
+        var start: Node? = null
+        for (i in 0..nodes.size()) {
+            val node = nodes[i]
+            val current = getPointFromIndex(i, grid)
+            if (current.x != 0) {
+                node.edges.add(nodes[getIndexFromPoint(Point(current.x - 1, current.y), grid)])
+            }
+            if (current.x < grid.columns - 1) {
+                node.edges.add(nodes[getIndexFromPoint(Point(current.x + 1, current.y), grid)])
+            }
+            if (current.y != 0) {
+                node.edges.add(nodes[getIndexFromPoint(Point(current.x, current.y - 1), grid)])
+            }
+            if (current.y < grid.rows - 1) {
+                node.edges.add(nodes[getIndexFromPoint(Point(current.x, current.y + 1), grid)])
+            }
+            if (node.marker.equals(Grid.Marker.START)) {
+                start = node
+            }
+        }
+        return start
+    }
+
+    private fun nodesToSet(nodes: ArrayList<Node>): TreeSet<Node> {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun findPath(startNode: Node, unvisited: TreeSet<Node>, grid: Grid): List<Point> {
+        val path = ArrayList<Point>()
+        return path
     }
 }
