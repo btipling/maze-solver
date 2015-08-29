a7585fd86cb199ad00d7ca55ada3b65d85436f61
Bjorn Tipling <bjorn@ambientchill.com>
2015-08-28 20:10:32 -0700

    Let's cache this number to make A* faster.

--- src/mazesolver/objects/algorithms/AStar.kt
+++ src/mazesolver/objects/algorithms/AStar.kt
@@ -12,13 +12,42 @@ public class AStar : Dijkstra() {
 
     inner class Node(marker: Grid.Marker, x: Int, y: Int) : Dijkstra.Node(marker, x, y) {
         public override var totalDistance: Double = Double.POSITIVE_INFINITY
-        set (distance: Double) {
+        public var projectedDistance: Double = Double.NEGATIVE_INFINITY
+        get() {
+            if (this.totalDistance.equals(Double.POSITIVE_INFINITY)) {
+                return this.totalDistance;
+            }
+            if ($projectedDistance > Double.NEGATIVE_INFINITY) {
+                return $projectedDistance
+            }
+            // Haven't calculated projected distance yet, doing it now, and only once.
             if (endPoint == null) {
                 throw Exception("There must be an endpoint!")
             }
             val ep = endPoint!!
             val manhattanDistance = Math.abs(this.x - ep.x) + Math.abs(this.y - ep.y)
-            $totalDistance = distance + manhattanDistance
+            $projectedDistance = totalDistance + manhattanDistance
+            return $projectedDistance
+        }
+
+        override fun compareTo(other: Dijkstra.Node): Int {
+            val n = other as Node
+            if (n.projectedDistance.equals(this.projectedDistance)) {
+                return 0
+            }
+            return if (this.projectedDistance < other.projectedDistance) -1 else 1
+        }
+
+        fun getNodeDistanceToEnd(node: Dijkstra.Node) : Double {
+            if (endPoint == null) {
+                throw Exception("There must be an endpoint!")
+            }
+            val ep = endPoint!!
+            if (node.totalDistance.equals(Double.POSITIVE_INFINITY)) {
+                return node.totalDistance;
+            }
+            val manhattanDistance = Math.abs(node.x - ep.x) + Math.abs(node.y - ep.y)
+            return node.totalDistance + manhattanDistance
         }
     }
 
